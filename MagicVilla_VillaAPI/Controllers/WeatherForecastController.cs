251986b187c332ae1d6897eb6b81a6f8620497b9
Dashrath1025 <dashrathkanzariya1025@gmail.com>
2023-06-20 10:49:13 +0530

    new added

--- MagicVilla_VillaAPI/Controllers/WeatherForecastController.cs
+++ MagicVilla_VillaAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace MagicVilla_VillaAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Clothing", "Brnewtestcing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
