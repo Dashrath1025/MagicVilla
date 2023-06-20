a81811107d33bd1c8555f87d681089dbaf2f5060
Dashrath kanzariya <91519946+Dashrath1025@users.noreply.github.com>
2023-06-20 18:00:19 +0530

    Update WeatherForecastController.cs

--- MagicVilla_VillaAPI/Controllers/WeatherForecastController.cs
+++ MagicVilla_VillaAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace MagicVilla_VillaAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Vs code", "from gitlab", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Pull locally", "from github", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
